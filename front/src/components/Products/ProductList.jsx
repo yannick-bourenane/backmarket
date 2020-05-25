aa5cf0bfc607f29dedb5cdc6ddec321d18f74b11
Pauline Morsli <pauline.morsli@gmail.com>
2020-05-25 11:33:52 +0200

    prodct list print all the data

--- front/src/components/Products/ProductList.jsx
+++ front/src/components/Products/ProductList.jsx
@@ -1,18 +1,41 @@
-import React from 'react'
-import {useSelector} from 'react-redux'
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 
 const ProductList = (props) => {
-    const products = useSelector(state => state.fetchAllPhones.data)
-let brandArray = new Set(products.map(brand =>brand.Brand))
-    // props.sales / props.number
-    return (
-        <div>
-        {console.log(brandArray)}
-        {/* {brandArray && brandArray.map(brand => <><input type="checkbox" id={brand} /> <label>{brand}</label></>)} */}
-        {console.log(products)}
-        {products.map(product => <li>{product.DeviceName}</li>)}
-        </div>
-    )
-}
+  const [brands, setBrands] = useState([]);
+  const products = useSelector((state) => state.fetchAllPhones.data);
+  let brandsPhones = [...new Set(products.map((brand) => brand.Brand))];
 
-export default ProductList
+  function filterByBrand(e, arr, brands) {
+    if (brands.includes(e.target.value)) {
+      return setBrands(brands.filter((m) => m !== e.target.value));
+    }
+    setBrands([...brands, e.target.value]);
+    
+  }
+
+  // props.sales / props.number
+  return (
+    <div>
+      {brandsPhones &&
+        brandsPhones.map((brand) => (
+          <>
+            <input
+              type="checkbox"
+              value={brand}
+              onChange={filterByBrand}
+              id={brand}
+            />{" "}
+            <label>{brand}</label>
+          </>
+        ))}
+      {products.map((product) => (
+        <li>{product.DeviceName}</li>
+      ))}
+
+      {console.log(brands)}
+    </div>
+  );
+};
+
+export default ProductList;
